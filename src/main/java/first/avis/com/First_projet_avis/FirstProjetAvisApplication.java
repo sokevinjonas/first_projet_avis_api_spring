81318c19e72094e51dd8688d885987c477e51ad2
JONAS SOW <142751744+sokevinjonas@users.noreply.github.com>
2024-05-15 02:30:46 +0200

    installation projet

---
+++ src/main/java/first/avis/com/First_projet_avis/FirstProjetAvisApplication.java
@@ -0,0 +1,13 @@
+package first.avis.com.First_projet_avis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjetAvisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjetAvisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/first/avis/com/First_projet_avis/FirstProjetAvisApplicationTests.java
@@ -0,0 +1,13 @@
+package first.avis.com.First_projet_avis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjetAvisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
